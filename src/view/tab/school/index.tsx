f0a5ca92808ea579ad8b6f9f1ec53546b24d7800
Veli Bacık <velibacik60@gmail.com>
2018-12-05 01:19:23 +0300

    card  view

--- src/view/tab/school/index.tsx
+++ src/view/tab/school/index.tsx
@@ -1,19 +1,43 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { Text, View, FlatList } from "react-native";
+import { Card, CardItem } from "native-base";
+import { styles } from "../../login/styles";
 
 export class SchoolsPage extends Component {
-
   componentWillMount = () => {
-    console.log(this.props)
+    console.log(this.props);
   };
-  
-  render() {
+  renderItem(val: string) {
     return (
-      <View>
-        <Text> Loading... </Text>
+      <Card>
+        <CardItem header bordered>
+          <Text style={{color :'red'}}>Player</Text>
+        </CardItem>
+        <CardItem>
+          <Text>{val}</Text>
+        </CardItem>
+      </Card>
+    );
+  }
+
+  render() {
+    var items = [
+      "Simon Mignolet",
+      "Nathaniel Clyne",
+      "Dejan Lovren",
+      "Mama Sakho",
+      "Emre Can"
+    ];
+
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={items}
+          renderItem={({ item }) => this.renderItem(item)}
+        />
       </View>
-    )
+    );
   }
 }
 
-export default SchoolsPage
+export default SchoolsPage;
